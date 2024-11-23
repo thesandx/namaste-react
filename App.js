3a9612194d9a4c94c52b1a135e34e8cf9bc671c5
thesandx <kumarjha.sandeep@gmail.com>
2024-11-23 21:14:20 +0530

    first lecture completed

---
+++ App.js
@@ -0,0 +1,22 @@
+// element injection using react
+    // so React.CreateElement takes 3 element and returns a object/json
+    const headingReact = React.createElement("h1", {id:"title"}, "3. Namaste React using React");
+    console.log(headingReact); // this will return a object
+
+    //here we get a global variable React which we got from the link which we donwload from cdn link
+    const heading1 = React.createElement("h1", {id:"heading1"}, "heading1");
+    const heading2 = React.createElement("h2", {id:"heading2"}, "heading2");
+    // if we want to render multiple elements then we can pass an array of elements to the React.createElement method
+    const container = React.createElement("div", {id:"container"}, [headingReact, heading1, heading2]);
+
+
+    // now we need to render this object into the root element
+    // so we will use ReactDOM.createRoot() method.
+    // When you pass a DOM element to createRoot(), such as a div with an id, React takes over that element completely. If there are any pre-existing child elements within that root div, it assumes full control and discards them because React manages its own component tree and rendering process.
+    //we will get below warning
+    //react.development.js:199 Warning: Each child in a list should have a unique "key" prop.
+    //so we need to pass a unique key to each element
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(container);
+    // this will render the object into the root element
+    //mostly we will have one root and one render method in our app no matter how big it is.
